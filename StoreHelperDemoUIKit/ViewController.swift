4622085afba6c15844d2efc7f776495602159270
Russell Archer <russell.archer@mac.com>
2022-12-13 14:13:02 +0000

    Experimental proof of concept for using StoreHelper in a UIKit project

--- StoreHelperDemoUIKit/ViewController.swift
+++ StoreHelperDemoUIKit/ViewController.swift
@@ -6,14 +6,30 @@
 //
 
 import UIKit
+import SwiftUI
+import StoreHelper
 
 class ViewController: UIViewController {
 
+    let appDelegate = UIApplication.shared.delegate as! AppDelegate
+    var productsView: UIHostingController<AnyView>?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        productsView = UIHostingController(rootView: AnyView(Products(productInfoCompletion: { id in
+
+        }).environmentObject(appDelegate.storeHelper)))
+        
+        if let productsView {
+            addChild(productsView)
+            view.addSubview(productsView.view)
+            productsView.view.translatesAutoresizingMaskIntoConstraints = false
+            productsView.view.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+            productsView.view.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+            productsView.view.leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true
+            productsView.view.rightAnchor.constraint(equalTo: view.rightAnchor).isActive = true
+        }
     }
-
-
 }
 
